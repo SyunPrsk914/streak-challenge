c5b1c391a30993feccd303375bd02115af44b063
SyunPrsk914 <syunsyun080914@gmail.com>
2026-04-13 00:08:14 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        warn(warning)
-      }
-    }
-  },
-  logLevel: 'info'
-})
+export default defineConfig({ plugins: [react()] })
